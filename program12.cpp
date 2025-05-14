16d5a10fd337d568fd66fed4873117fd0cb0b81f
Biswajit Ball <103375895+BiswajitBall@users.noreply.github.com>
2025-05-14 19:23:52 -0400

    Create program12.cpp

---
+++ program12.cpp
@@ -0,0 +1,17 @@
+// for loop (C++ programming)
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  //int n(100);
+
+  for (int n=100; n>0; n--) //for (initialization; condition; increase)
+  {
+    cout << n << ", ";
+
+  }
+
+  cout << "liftoff! \n";
+}
