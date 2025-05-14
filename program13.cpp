9c2f7c7269f5605e845c6e38c5075f6fef971bb8
Biswajit Ball <103375895+BiswajitBall@users.noreply.github.com>
2025-05-14 19:37:00 -0400

    Create program13.cpp

---
+++ program13.cpp
@@ -0,0 +1,21 @@
+// for loop (C++ programming)
+#include <iostream>
+
+using namespace std;
+
+//const char tab = '\t';
+
+int main ()
+{
+
+  int n, i;
+
+  for (n=100, i=0; n!=i; n--, i++) //for (initialization; condition; increase)
+  {
+    cout << n << "|" << i << ", ";
+
+  }
+
+  cout << "liftoff! \n";
+
+}
