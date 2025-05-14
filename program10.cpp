252b7eccb0467701e43a6ce9d7b6b242e0e6a29e
Biswajit Ball <103375895+BiswajitBall@users.noreply.github.com>
2025-05-14 19:02:36 -0400

    Create program10.cpp

---
+++ program10.cpp
@@ -0,0 +1,15 @@
+// while loop  (C++ programming)
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+   int n(10);
+
+   while (n > 0)
+   {
+     cout << n << ", ";
+     --n;
+   }
+   cout << "liftoff!\n";
+}
