9778fcbfc520ac7900e5d320ad5e7becf62aa326
Biswajit Ball <103375895+BiswajitBall@users.noreply.github.com>
2025-05-14 18:33:27 -0400

    Create program9.cpp

---
+++ program9.cpp
@@ -0,0 +1,25 @@
+// if - else if - else (C++ programming)
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int X{20};
+  int Y = 30;
+
+  if (X == Y)
+  {
+    cout << "X is equal to Y" << endl;
+  }
+  else if (X > Y)
+  {
+    cout << "X is greater than Y" << endl;
+  }
+  else
+  {
+    cout << "X is less than Y" << endl;
+  }
+
+
+  cout << "Example of If - Else If - Else Statement in C++" << endl;
+}
