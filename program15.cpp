a4bab4cde466b2469ddb043802ef1e730cdea294
Biswajit Ball <103375895+BiswajitBall@users.noreply.github.com>
2025-08-03 23:42:47 -0400

    Update program15.cpp

--- program15.cpp
+++ program15.cpp
@@ -1,1 +1,18 @@
+// nested loop (C++ programming)
+#include <iostream>
+using namespace std;
 
+int main()
+{
+  int i, j;
+  for (i=1; i<=5; i++)
+  {
+   cout << "\n Pass" << "-" << i << ":";
+   for (j=1; j<=5; j++)
+   {
+    cout << " " << j;
+   }
+
+  }
+  cout << "\n";
+}
